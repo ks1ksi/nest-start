15334fcd40b383ea7be17e2f29516a7c43e5f31b
Seungil Kim <ksi990302@gmail.com>
2023-06-12 18:47:57 +0900

    add auth

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,20 @@
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({
+      username,
+      password,
+    });
+    await this.save(user);
+  }
+}
