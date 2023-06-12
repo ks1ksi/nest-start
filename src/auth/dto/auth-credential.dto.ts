13c81844391119c2d20dd3650475aaa4166c254e
Seungil Kim <ksi990302@gmail.com>
2023-06-12 19:01:20 +0900

    add validation

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,5 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // Passwords will contain at least 1 alphabet, 1 number and 1 special character
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@!%*#?&])[A-Za-z\d$@!%*#?&]{8,}$/, {
+    message: 'Password must contain at least 1 special character',
+  })
   password: string;
 }
