c699ec7c54889d429abaa42172c5907d253d6904
Seungil Kim <ksi990302@gmail.com>
2023-06-18 22:58:32 +0900

    Feat: add jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,21 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import 'dotenv/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      // 유저 토큰 생성 (secret, payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      return { accessToken: accessToken };
     } else {
       throw new UnauthorizedException('Login failed');
     }
